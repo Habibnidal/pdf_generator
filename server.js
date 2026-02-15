65c218ee24c1bfeb66c1d88df39b2879c53ad43a
Habibnidal <habibnidalnasser@gmail.com>
2026-02-15 21:55:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const fs = require("fs");
-const puppeteer = require("puppeteer");
-
 const app = express();
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
